7fa31bfe6cf04729fa81a4c90333b801bc1691b4
madhumithabaskar95 <63431745+madhumithabaskar95@users.noreply.github.com>
2025-07-10 16:48:30 -0400

    Initial Commit

---
+++ src/test/java/com/io/org/productinventory/ProductinventoryApplicationTests.java
@@ -0,0 +1,14 @@
+package com.io.org.productinventory;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductinventoryApplicationTests {
+
+	@Disabled
+	void contextLoads() {
+	}
+
+}
